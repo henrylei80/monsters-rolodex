91eecc4c0b7ee0015160e43e6b6afef6ece96026
Hong <henrylei80@yahoo.com>
2019-10-05 13:22:08 -0400

    Updates

---
+++ precache-manifest.4466f74b42e0bb91b29037dad9a41575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf62b11058c2d83e22f2439193fc78c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38e33abf7a4bbe91facb",
+    "url": "/monsters-rolodex/static/css/main.478f90db.chunk.css"
+  },
+  {
+    "revision": "5d36f9edd2e8239827bd",
+    "url": "/monsters-rolodex/static/js/2.2e2b55ad.chunk.js"
+  },
+  {
+    "revision": "38e33abf7a4bbe91facb",
+    "url": "/monsters-rolodex/static/js/main.98b11f1c.chunk.js"
+  },
+  {
+    "revision": "78038fd504a1eb8974b8",
+    "url": "/monsters-rolodex/static/js/runtime-main.e34c7019.js"
+  }
+]);
